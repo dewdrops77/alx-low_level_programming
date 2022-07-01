e19693570dbdf5b03637856c6a047342fe5f6d2f
dewdrops77 <leonnieokojie@gmail.com>
2022-07-01 09:58:56 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,3 +9,10 @@ int main(void)
 int numb;
 char alphab;
 
+for (numb = 48 ; numb <= 57 ; numb++)
+putchar(numb);
+for (alphab = 97 ; alphab <= 102 ; alphab++)
+putchar(alphab);
+putchar('\n');
+return (0);
+}
