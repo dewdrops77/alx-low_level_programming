d2daa243fc20bd6c50784d12b130fe1388db1571
dewdrops77 <107578166+dewdrops77@users.noreply.github.com>
2022-07-06 11:40:49 +0400

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,17 +7,17 @@ void print_to_98(int n);
 int add(int, int);
 void times_table(void);
 void jack_bauer(void);
-int print_last_digit(int)
-int print_last_digit(int);
-#include "main.h"
 
-/**
-* _abs - a function that computes the absolute value of an integer
-* @ab: integer input
-* Return: absolute value of ab
-*/
-int _abs(int ab)
-{
-if (ab < 0)
-ab = aint _abs(int);
-int _abs(int);
+
+
+
+
+
+
+
+
+
+
+
+
+
