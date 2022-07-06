9b1b26b67f2daf085ca0373d915b2c559206a3c1
dewdrops77 <leonnieokojie@gmail.com>
2022-07-06 02:07:26 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
-* main - print putchar
-* Return: Always 0
+* main - Entry point
+*
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-putchar(');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
+ *sh = "Holberton";
+while (*sh)
+{
+_putchar(*sh);
+sh++;
+}
 _putchar('\n');
 
-return(0);
+return (0);
 }
