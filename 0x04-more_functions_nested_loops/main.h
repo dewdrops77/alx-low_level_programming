a52ff9747b9dd81b97403753ec381c96838ac015
dewdrops77 <leonnieokojie@gmail.com>
2022-07-07 07:51:16 -0700

    :Task 5

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,1 @@
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-int mul(int a, int b);
-int mul(int a, int b);
-void print_number(int n);
-void print_triangle(int size);
-void print_numbers(void);
-void print_most_numbers(void);
 void more_numbers(void);
-void print_numbers(void);
-void print_most_numbers(void);
