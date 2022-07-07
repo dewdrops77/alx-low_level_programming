ec820094903d9190b2740ed1fcf974f1ae5e24da
dewdrops77 <leonnieokojie@gmail.com>
2022-07-07 07:40:09 -0700

    Task 3

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,3 +6,6 @@ int mul(int a, int b);
 void print_number(int n);
 void print_triangle(int size);
 void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_numbers(void);
