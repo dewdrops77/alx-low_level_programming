070640244bd20ad1a7a02b99d9bc82030225ba69
dewdrops77 <leonnieokojie@gmail.com>
2022-07-12 06:08:58 -0700

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,44 +1,20 @@
 #include "main.h"
-#include <stdio.h>
+#include "string.h"
+
 /**
- * _atoi - print the integer of a char.
- * @s:  tested char
- * Return: integer.
+ * *_strcpy - main function
+ * @dest: parameter
+ * @src: parameter
+ *Return: destination
  */
-int _atoi(char *s)
+char *_strcpy(char *dest, char *src)
 {
-	unsigned int counter, i, j, k, length, num, l;
-	int aux;
+int b;
 
-	aux = 1;
-	counter = 0;
-	num = 0;
-
-	while (*(s + counter) != '\0')
-		counter++;
-	for (i = 0; i < counter; i++)
-	{
-		if (*(s + i) <= '9' && *(s + i) >= '0')
-		break;
-	}
-	for (j = i; j < counter; j++)
-	{
-		if (!(*(s + j) <= '9' && *(s + j) >= '0'))
-			break;
-	}
-
-	for (k = 0; k < i; k++)
-	{
-		if (*(s + k) == '-')
-			aux = -aux;
-	}
-	length = j - i;
-	l = i;
-		while (length >= 1)
-	{
-		num = num * 10 + (*(s + l) - '0');
-		l++;
-		length--;
-	}
-	return (num * aux);
+for (b = 0; src[b]; b++)
+{
+dest[b] = src[b];
+}
+dest[b] = src[b];
+return (dest);
 }
