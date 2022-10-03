969ac1aae6040c1f252332b508d5c8d236b86487
root <okojieleonnie8@gmail.com>
2022-10-03 12:58:42 -0700

    win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/dewdrops77/alx-low_level_programming/raw/master/0x18-dynamic_libraries/101-make_me_win.sh
-export LD_PRELOAD=/tmp/101-make_me_win.sh
+wget -O /tmp/win.so https://github.com/dewdrops77/alx-low_level_programming/raw/master/0x18-dynamic_libraries/win.so
+export LD_PRELOAD=/tmp/win.so
